aa3d8bd9411b507fdaff397b5febeef21180f06d
egaeuni <gaeun3619@gmail.com>
2025-04-26 14:36:46 +0900

    7. todo ê객체에입력받은 내용 채우기

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -20,6 +20,10 @@ public class App {
                     System.out.print("할 일: ");
                     String content = scanner.nextLine().trim();
 
+                    Todo todo = new Todo(id, content);
+                    System.out.println(todo.getContent());
+                    System.out.println(todo.getId());
+
                     System.out.printf("%d번 할 일이 생성되었습니다.\n", id);
                 }
             }
